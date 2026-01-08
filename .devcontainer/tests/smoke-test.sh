6521751e83f69cea5b113b2825260a0b890fcb16
John Martin <john.itvn@gmail.com>
2026-01-08 18:24:47 +0000

    ci: refactor test scripts

---
+++ .devcontainer/tests/smoke-test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+bash "${CURRENT_DIR}/cmd-exits.sh" \
+    yq jq unzip zip git ping zsh \
+    go node npm yarn pnpm dprint docker \
+    shellcheck lefthook conform shfmt pants \
+    kubectl helm k3d minikube kustomize kubeseal \
+    terraform ansible ansible-lint \
+    actionlint golangci-lint
